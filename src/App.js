ea7c1a2cda9b282a9671b577e8f9e6b55a7bea8b
David Szabo <david.szabo97@gmail.com>
2020-03-24 21:00:19 +0100

    live coding 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,192 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState, useEffect } from 'react';
 import './App.css';
+import chatClient from './ChatClient'
+
+const formatTimestamp = (ts) => {
+  const date = new Date(ts)
+  return `${date.getHours()}:${date.getMinutes()}`
+}
+
+const useChat = () => {
+  const [ , setConnected ] = useState(chatClient.connected)
+
+  useEffect(() => {
+    chatClient.on('connected', () => setConnected(chatClient.connected))
+    chatClient.on('disconnected', () => setConnected(chatClient.connected))
+  }, []);
+
+  return { client: chatClient }
+}
+
+const useChatRooms = () => {
+  const [ rooms, setRooms ] = useState([])
+
+  useEffect(() => {
+    chatClient.onMessage('ROOM_LIST', ({ payload }) => setRooms(payload))
+  }, [])
+
+  return rooms
+}
+
+const useChatUsers = () => {
+  const [ users, setUsers ] = useState([])
+
+  useEffect(() => {
+    chatClient.onMessage('USER_LIST', ({ payload }) => setUsers(payload))
+  }, [])
+
+  return users
+}
+
+const useChatMessages = () => {
+  const [ messages, setMessages ] = useState([])
+
+  useEffect(() => {
+    chatClient.onMessage('MESSAGE_LIST', ({ payload }) => setMessages(payload))
+    chatClient.onMessage('ROOM_MESSAGE', ({ payload }) => setMessages((messages) => [...messages, payload]))
+  }, []);
+
+  return messages
+}
+
+const RoomList = ({ items, activeItemId, onItemClick, ...props }) => {
+  const handleRoomListItemClick = useCallback((id) => {
+    const item = items.find(item => item.id === id)
+    onItemClick(item)
+  }, [ items, onItemClick ])
+
+  return (
+    <div className="RoomList" {...props}>
+      {items.map(({ id, ...props }) => (
+        <RoomListItem key={id} active={id === activeItemId} id={id} {...props} onClick={handleRoomListItemClick} />
+      ))}
+    </div>
+  )
+}
+
+const RoomListItem = ({ id, active, name, onClick }) => {
+  const handleOnClick = useCallback(() => {
+    if (typeof onClick !== 'function') {
+      return
+    }
+
+    onClick(id)
+  }, [ onClick, id ])
+
+  return (
+    <div className={`RoomListItem ${active ? 'RoomListItem--Active' : ''}`} onClick={handleOnClick}>{name}</div>
+  )
+}
+
+const UserList = ({ items, ...props }) => {
+  return (
+    <div className="UserList" {...props}>
+      {items.map(({ id, ...props }) => (
+        <UserListItem key={id} id={id} {...props} />
+      ))}
+    </div>
+  )
+}
+
+const UserListItem = ({ name }) => {
+  return <div className="UserListItem">{name}</div>
+}
+
+const MessageList = ({ items, ...props }) => {
+  return (
+    <div className="MessageList" {...props}>
+      {items.map(({ id, ...props }) => (
+        <MessageListItem key={id} id={id} {...props} />
+      ))}
+    </div>
+  )
+}
+
+const MessageListItem = ({ from, content, createdAt }) => {
+  return (
+    <div className="MessageListItem">
+      <span className="MessageListItem__From">{from}</span> <span className="MessageListItem__CreatedAt">{formatTimestamp(createdAt)}</span>
+      <div className="MessageListItem__Content">{content}</div>
+    </div>
+  )
+}
+
+const TextInput = ({ value, onEnter, ...props }) => {
+  const handleKeyUp = useCallback((event) => {
+    if (event.keyCode !== 13) {
+      return
+    }
+
+    if (typeof onEnter !== 'function') {
+      return
+    }
+
+    onEnter(event.target.value)
+  }, [ onEnter ])
+
+  return <input type="text" value={value} onKeyUp={handleKeyUp} {...props} />
+}
 
 function App() {
+  const { client } = useChat()
+  const [name, setName] = useState(null)
+  const [currentRoomId, setCurrentRoomId] = useState(null)
+  const rooms = useChatRooms()
+  const users = useChatUsers()
+  const messages = useChatMessages()
+
+  const handleItemClick = (item) => {
+    client.joinRoom(item.name)
+    setCurrentRoomId(item.id)
+  }
+
+  const handleOnNameEnter = useCallback((name) => {
+    if (!name) {
+      return
+    }
+
+    client.setName(name)
+    setName(name)
+  }, [client])
+
+  const handleOnMessageEnter = useCallback((content) => {
+    if (!content) {
+      return
+    }
+
+    client.sendMessage(content)
+  }, [client])
+
+  if (!client.connected) {
+    return <div>Connecting...</div>
+  }
+
+  if (!name) {
+    return (
+      <div className="NameModal">
+        <p style={{fontSize:'3rem', color: '#fff', textAlign: 'center', marginBottom: '2rem'}}>What's your name?</p>
+        <TextInput className="NameTextInput" onEnter={handleOnNameEnter} />
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="LeftColumn">
+        <RoomList items={rooms} activeItemId={currentRoomId} onItemClick={handleItemClick} />
+      </div>
+
+      <div className="MiddleColumn">
+        <MessageList items={messages} />
+
+        <div style={{marginTop: 'auto'}}>
+          <TextInput className="MessageTextInput" placeholder="Type something awesome..." onEnter={handleOnMessageEnter} />
+        </div>
+      </div>
+
+      <div className="RightColumn">
+        <UserList items={users} />
+      </div>
     </div>
   );
 }
---
+++ src/ChatClient.js
@@ -0,0 +1,109 @@
+class ChatClient {
+
+  constructor(wsUrl) {
+    this.connected = false
+    this.listeners = {}
+
+    this.socket = new WebSocket('ws://localhost:8080');
+
+    this.socket.addEventListener('open', (event) => {
+      this._onConnected(event)
+    });
+
+    this.socket.addEventListener('message', (event) => {
+      let message = null
+
+      try {
+        message = JSON.parse(event.data)
+      } catch (err) {
+        console.error(err)
+        return
+      }
+
+      if (message === null) {
+        return
+      }
+
+      if (typeof message.type !== 'string') {
+        console.error('Received invalid message', message)
+        return
+      }
+
+      this._onMessage(message)
+    })
+  }
+
+  _onConnected() {
+    this.connected = true
+    console.log('Connected to server')
+    this.emit('connected')
+  }
+
+  _onDisconnected() {
+    this.emit('disconnected')
+  }
+
+  _onError(err) {
+    this.emit('error', err)
+  }
+
+  _onMessage(message) {
+    console.log('Message from server ', message);
+    this.emit('message', message)
+  }
+
+  onMessage(type, callback) {
+    this.on('message', (message) => {
+      if (message.type !== type) {
+        return
+      }
+
+      callback(message)
+    })
+  }
+
+  send(type, payload) {
+    let message = { type, payload }
+
+    if (typeof payload === 'undefined') {
+      if (typeof type.type !== 'undefined') {
+        message = type
+      }
+    }
+
+    this.socket.send(JSON.stringify(message))
+  }
+
+  on(event, callback) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = []
+    }
+
+    this.listeners[event].push(callback)
+  }
+
+  emit(event, ...args) {
+    const listeners = this.listeners[event] || []
+    listeners.forEach((listener) => listener(...args))
+  }
+
+  setName(name) {
+    this.send('SET_NAME', name)
+  }
+
+  joinRoom(id) {
+    this.send('JOIN_ROOM', id)
+  }
+
+  leaveRoom(id) {
+    this.send('LEAVE_ROOM', id)
+  }
+
+  sendMessage(content) {
+    this.send('SEND_MESSAGE', content)
+  }
+}
+
+const chatClient = new ChatClient()
+
+export default chatClient
